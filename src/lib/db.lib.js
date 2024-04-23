2561a5b50688c3b8e5f7668696a138f6e1c76039
wisnu31899 <wisnuhidayat318@gmail.com>
2024-04-23 10:35:30 +0700

    feat: eslint

--- src/lib/db.lib.js
+++ src/lib/db.lib.js
@@ -1,15 +1,15 @@
 const pg = require('pg')
 
 const db = new pg.Pool({
-    // // localhost
-    // connectionString: "postgresql://postgres:1@localhost:5432/monolith?sslmode=disable"
-    
-    // // db docker
-    connectionString: 'postgresql://postgres:1@db:5432/postgres?sslmode=disable'
+  // // localhost
+  // connectionString: "postgresql://postgres:1@localhost:5432/monolith?sslmode=disable"
+
+  // // db docker
+  connectionString: 'postgresql://postgres:1@db:5432/postgres?sslmode=disable'
 })
 
-db.on("connect", ()=>{
-    console.log("DB Connected")
+db.on('connect', () => {
+  console.log('DB Connected')
 })
 
 module.exports = db
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,19 +1,19 @@
 const userController = require('../controllers/users.controller')
 const router = require('express').Router()
 
-router.get('/', (req, res)=>{
-    return res.render('index', {title:null})
+router.get('/', (req, res) => {
+  return res.render('index', { title: null })
 })
 
 router.use('/users', require('./users.router'))
 
-router.get('/insert-user', (req, res)=>{
-    return res.render('users/insert', {title: "insert"})
+router.get('/insert-user', (req, res) => {
+  return res.render('users/insert', { title: 'insert' })
 })
 router.post('/insert-user', userController.insertUser)
 
-router.get('/about-us', (req, res)=>{
-    return res.render('about-us', {title: "About Us"})
+router.get('/about-us', (req, res) => {
+  return res.render('about-us', { title: 'About Us' })
 })
 
 module.exports = router
